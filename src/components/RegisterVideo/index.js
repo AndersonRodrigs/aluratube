76858ed4f4f2ca420c419be66f0c8e956ecf6055
Anderson Rodrigues <105132452+AndersonRS080@users.noreply.github.com>
2022-11-11 19:34:51 -0300

    Atualização aula 4

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyledRegisterVideo } from "./styles"
+import { StyledRegisterVideo } from "./Styles"
 
 function userForm(propsDoForm) {
   const [values, setValues] = React.useState(propsDoForm.initialValues)
